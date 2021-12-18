c9b43f75e6838b58b0ded5c9cf2abf4fad877560
Iskren Ivanov <pecito_vn@hotmail.com>
2021-12-18 13:49:58 +0200

    Initial commit

---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,20 @@
+import './TopBar.css';
+
+
+const TopBar = () => {
+    return (
+        <div className="top">
+            <div className='topLeft'>
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-instagram-square"></i>
+                <i class="fab fa-pinterest-square"></i>
+                <i class="fab fa-twitter-square"></i>
+            </div>
+            <div className='center'>c</div>
+            <div className='topRight'>r</div>
+        </div>
+    );
+};
+
+
+export default TopBar;
