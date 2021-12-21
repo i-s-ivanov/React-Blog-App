b0e537d1b5c420497e57456b6ac672b8c4a814e0
Iskren Ivanov <pecito_vn@hotmail.com>
2021-12-21 20:37:47 +0200

    Router added

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -3,7 +3,7 @@ import './TopBar.css';
 
 
 const TopBar = () => {
-    const user = true;
+    const user = false;
     return (
         <div className="top">
             <div className='topLeft'>
@@ -18,11 +18,20 @@ const TopBar = () => {
                     <li className="topListItem"><Link className='link' to={"/about"}>ABOUT</Link></li>
                     <li className="topListItem"><Link className='link' to={"/contact"}>CONTACT</Link></li>
                     <li className="topListItem"><Link className='link' to={"/write"}>WRITE</Link></li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">{user ? "LOGOUT" : ''}</li>
                 </ul>
             </div>
             <div className='topRight'>
-                <img className="topImg" src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                {
+                    user ? (
+                        <img className="topImg" src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                    ) : (
+                        <ul className='topList'>
+                            <li className="topListItem"><Link className='link' to={"/login"}>LOGIN</Link></li>
+                            <li className="topListItem"><Link className='link' to={"/register"}>REGISTER</Link></li>
+                        </ul>
+                    )
+                }
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
